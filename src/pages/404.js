a1e7555e9cb497929b7c78721506a9a5d5ed82f6
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-18 07:42:34 -0400

    stub layout and nav

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>
+        This page doesn't exist
+      </h1>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Layout from '../components/Layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Full Stack Software Development</h2>
+          <h2>Develop & Deploy</h2>
+          <p>Full Stack Web Developer based in Philadelphia</p>
+        </div>
+      </section>
+    </Layout>
+  )
 }
